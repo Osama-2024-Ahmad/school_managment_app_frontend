4fb7280a3e29db90d4a2265c61d4a961157a3e42
osama-2024-ahmad <osama-2024-ahmad@hotmail.com>
2025-12-03 20:41:34 +0100

    Initialize core application files: App entry point, main styles, and routing structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import TeacherDetails from './pages/details/TeacherDetails';
+import ProgramDetails from './pages/details/ProgramDetails';
+import EventDetails from './pages/details/EventDetails';
+import TestimonialForm from './pages/TestimonialForm';
+import Grades from './pages/Grades';
+import ProgramsPage from './pages/ProgramsPage';
+import EventsPage from './pages/EventsPage';
+import TeachersPage from './pages/TeachersPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/grades" element={<Grades />} />
+        <Route path="/teachers" element={<TeachersPage />} />
+        <Route path="/teachers/:id" element={<TeacherDetails />} />
+        <Route path="/programs" element={<ProgramsPage />} />
+        <Route path="/programs/:id" element={<ProgramDetails />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/events/:id" element={<EventDetails />} />
+        <Route path="/testimonial" element={<TestimonialForm />} />
+
+
+
+      </Routes>
+
+
+
     </div>
   );
 }
